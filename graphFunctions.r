691e5a3f11f9c4d2362013aa6a9842af81445c38
Tim Nicholson-Shaw <timnicholsonshaw@gmail.com>
2021-04-15 13:49:06 -0700

    Began R Plotting Code

---
+++ graphFunctions.r
@@ -0,0 +1,17 @@
+library(ggplot2)
+
+read_in_prepped_sam <- function(csv_file) {
+    df <- read_csv(csv_file)
+
+    df <- select(df, -QNAME, -RNAME, -MAPQ, -CIGAR, -RNEXT, -PNEXT, -TLEN, -QUAL, -FLAG, -SEQ) #drop unnecessary columns
+
+    df <- pivot_longer(df, cols=c(RIGHT,LENGTH), names_to="ATTRIBUTE", values_to="VALUE")
+    return(df)
+}
+
+plot_lengths <- function(df, sample) {
+
+    filter(df, SAMPLE==sample, ATTRIBUTE=="LENGTH") %>%
+        ggplot(aes(x=VALUE)) + geom_density()
+
+}
