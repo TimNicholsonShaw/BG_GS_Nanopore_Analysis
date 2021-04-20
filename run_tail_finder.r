fa0124c8ba53cf3916760d80d261e5a64088eb00
Tim Nicholson-Shaw <timnicholsonshaw@gmail.com>
2021-04-20 16:16:26 -0700

    Began code for tailfindr stuff

--- run_tail_finder.r
+++ run_tail_finder.r
@@ -1,3 +1,16 @@
 library(tailfindr)
 
+# error with setting up compute clusters
+# try running 
+# parallel:::setDefaultClusterOptions(setup_strategy = "sequential")
+
 # Code with all the setting necessary to quickly run the tailfindr library
+
+# test code
+tailfindr_test <- function(){
+    df <- find_tails(fast5_dir = system.file('extdata', 'cdna', package = 'tailfindr'),
+                 save_dir = '~/Downloads',
+                 csv_filename = 'cdna_tails.csv',
+                 num_cores = 4)
+    return(df)
+}
