ae1ab106894ab41a81fe93d69ecaca00bc989f0e
Tim Nicholson-Shaw <timnicholsonshaw@gmail.com>
2021-04-21 13:09:52 -0700

    modified shell scripts

--- shell_scripts/guppy_runner.sh
+++ shell_scripts/guppy_runner.sh
@@ -1,18 +1,21 @@
 #! /bin/csh
 #PBS -q hotel
 #PBS -N GUPPY
-#PBS -l nodes=1:ppn=12
+#PBS -l nodes=1:ppn=4
 #PBS -l walltime=04:00:00
 #PBS -A jlykkeandersen
 #PBS -V
-#PBS -o sambamout_$SAM
-#PBS -e sambamerror_$SAM
+#PBS -o guppy.o
+#PBS -e guppy.e
 
-/home/t2shaw/ont-guppy-cpu/binguppy_basecaller \
+/home/t2shaw/ont-guppy-cpu/bin/guppy_basecaller \
 -i $INPUTPATH \
 -s $SAVEPATH \
 -c dna_r9.4.1_450bps_fast.cfg \
 --recursive \
 --barcode_kits SQK-PBK004 \
---trim_barcodes \
 --fast5_out\
+--cpu_threads_per_caller 2 \
+--num_callers 2 \
+--trim_strategy none
+
